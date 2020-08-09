cc9c1f1d9f49f118ab67df9a149b3f9d1936eed2
Sesath_De_Costa <sesathdecosta@gmailcom>
2020-08-09 11:13:15 +0530

    Added Student UI

---
+++ V1/src/lk/ijse/schoolSystem/controller/StudentFormController.java
@@ -0,0 +1,14 @@
+package lk.ijse.schoolSystem.controller;
+
+import javafx.event.ActionEvent;
+
+public class StudentFormController {
+    public void btnSaveActionPeformed(ActionEvent actionEvent) {
+    }
+
+    public void btnUpdateActionPeformed(ActionEvent actionEvent) {
+    }
+
+    public void btnDeleteActionPeformed(ActionEvent actionEvent) {
+    }
+}
